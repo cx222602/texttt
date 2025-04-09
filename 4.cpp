1d72d9ba268377142ad8934f7a5c5586179f1b05
cx222602 <cx222_2024@qq.com>
2025-04-09 23:24:04 +0800

    feat: 增加hello world2

---
+++ 4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<stack>
+#include<iomanip>
+#include<string>
+#include<stdio.h>
+using namespace std;
+int main(){
+
+
+    cout<<"Hello world"<<endl;
+    return 0;
+}
